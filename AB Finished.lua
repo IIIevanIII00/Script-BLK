c75097bc85713330b37acd48113a4b76b689c5bc
Galaxy <108278887+IIIevanIII00@users.noreply.github.com>
2025-01-23 03:40:50 +0100

    Update AB Finished.lua

--- AB Finished.lua
+++ AB Finished.lua
@@ -1,1 +1,26 @@
+<html>
 
+<head>
+    <link rel="stylesheet" href="Website Style.css">
+</head>
+
+<body>
+    <div class="Mainframe">
+        <img src="https://i.pinimg.com/564x/88/c5/8a/88c58a1e61aa138aab1ea516961f307b.jpg"
+            alt="Fuckin imagine refused to load" width="108" height="98" class="HubLogo">
+        <h1 class="TopCenter long-text">BLK_Galaxie's Project's & Script's Manager</h1>
+        <a href="https://discord.gg/QZEay5gczt">
+            <button class="Discord long-text">Join Server BLK_SCRIPT</button>
+        </a>
+        <h1 class="ScriptTextLabel">Script Download's</h1>
+        <div class="ScriptDownloader">
+            <a href="https://downgit.github.io/#/home?url=https://github.com/IIIevanIII00/Script2025/blob/main/the%20real%20inf.lua">
+                <button class="DefaultButton">AB Finished</button>
+            </a>
+            <a href="https://downgit.github.io/#/home?url=https://github.com/IIIevanIII00/Script2025/blob/main/AB%20Finished.lua">
+                  <button class="DefaultButton">The Real Inf</button>
+        </div>
+    </div>
+</body>
+
+</html>
